c49b6caf39cccb9a72336090ab740f42edd0c886
jaerbi <jaerbi42@gmail.com>
2017-06-30 21:42:44 +0000

    min fixed marg padd

--- js/index.js
+++ js/index.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
-    
-// for skills progress animations
+
+    // for skills progress animations
     $(window).scroll(function() {
         if ($(this).scrollTop() > 1690) {
             $('#skills__list').removeClass('skills__list');
         }
     });
-    
-// Text animation slowly appeared
+
+    // Text animation slowly appeared
     $(window).scroll(function() {
         if ($(this).scrollTop() > 2200) {
-            
-            for (var i = 1; i <= 12; i++) {
-                $('.content .exp_slowmo:nth-child(' + i + ')').delay(1000 + (i * 650)).animate({
+            for (var i = 1; i <= 6; i++) {
+                $('.exp_slowmo:nth-child(' + i + ')').delay(100 + (i * 545)).animate({
                     opacity: 1,
                 }, "slow");
             }
         }
     });
-    
+
 });
