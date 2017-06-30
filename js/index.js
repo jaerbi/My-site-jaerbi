cbdf0c5fce8679323af012bfe1b8a1a45d180f51
jaerbi <jaerbi42@gmail.com>
2017-06-30 21:25:02 +0000

    js jQuery animation text

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
+$(document).ready(function() {
+    
 // for skills progress animations
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 1690) {
-        $('#skills__list').removeClass('skills__list');
-    }
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1690) {
+            $('#skills__list').removeClass('skills__list');
+        }
+    });
+    
+// Text animation slowly appeared
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 2200) {
+            
+            for (var i = 1; i <= 12; i++) {
+                $('.content .exp_slowmo:nth-child(' + i + ')').delay(1000 + (i * 650)).animate({
+                    opacity: 1,
+                }, "slow");
+            }
+        }
+    });
+    
 });
