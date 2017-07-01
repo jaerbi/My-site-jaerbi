4d57d62cd747f63d3bdb3e3721d842fd30baf3fc
jaerbi <jaerbi42@gmail.com>
2017-07-01 20:56:11 +0000

    exp-mobil responsive

--- js/index.js
+++ js/index.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
 
+    const WINDOW_MAX_HEIGHT_Skills = 1690;
+    const WINDOW_MAX_HEIGHT_Exp = 2200;
+    const NUM_OF_EXP = 12;
+    const DELAY_TIME = 545;
+    
     // for skills progress animations
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 1690) {
+        if ($(this).scrollTop() > WINDOW_MAX_HEIGHT_Skills) {
             $('#skills__list').removeClass('skills__list');
         }
     });
 
-    // Text animation slowly appeared
+    // Text animation slowly to show up
     $(window).scroll(function() {
-        if ($(this).scrollTop() > 2200) {
-            for (var i = 1; i <= 6; i++) {
-                $('.exp_slowmo:nth-child(' + i + ')').delay(100 + (i * 545)).animate({
+        if ($(this).scrollTop() > WINDOW_MAX_HEIGHT_Exp) {
+            for (var i = 1; i <= NUM_OF_EXP; i++) {
+                $('.exp_slowmo:nth-child(' + i + ')').delay(i * DELAY_TIME).animate({
                     opacity: 1,
                 }, "slow");
             }
