0a9b8e6786c130e5c4d5d4813532faa2bb5cc415
jaerbi <jaerbi42@gmail.com>
2017-07-02 21:14:50 +0000

    background fixed

--- js/form.js
+++ js/form.js
@@ -5,17 +5,16 @@ $(function() {
 function sendForm(e) {
     e.preventDefault();
     $.ajax({
-        url: "https://formspree.io/jaerbi42@gmail.com",
-        method: "POST",
-        data: {
-            name: $('#user-name').val(),
-            email: $('#user-email').val(),
-            wish: $('#user-wish').val()
-        },
-        dataType: "json"
-    })
-    .done(function() {
-        alert('Дякую за ваше повідомлення');
-    });
+            url: "https://formspree.io/jaerbi42@gmail.com",
+            method: "POST",
+            data: {
+                name: $('#user-name').val(),
+                email: $('#user-email').val(),
+                wish: $('#user-wish').val()
+            },
+            dataType: "json"
+        })
+        .done(function() {
+            alert('Дякую за ваше повідомлення');
+        });
 }
-
